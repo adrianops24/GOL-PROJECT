c12f274c5bbd61bf0e1998e27ae5765b9404563a
adrianops24 <adrianpsilva90@gmail.com>
2026-02-15 23:16:16 -0300

    :sparkles: feat: add method grid

---
+++ src/gameOfLife.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.random.RandomGenerator;
+
+public class gameOfLife {
+
+    int[][] grid;
+
+    public gameOfLife(int sizeW, int sizeH) {
+        grid = new int[sizeW][sizeH];
+    }
+
+    public void exibir() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
---
+++ src/main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class main {
+
+
+
+    public void main() {
+        //chamada de metodo
+        Scanner enter = new Scanner(System.in);
+        int sizeW = enter.nextInt(); //columns
+        int sizeH = enter.nextInt(); //lines
+        int sizeG = enter.nextInt(); //generates
+
+        boolean[][] cell = new boolean[1][0];
+
+    }
+
+
+}
